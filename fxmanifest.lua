9b31334525456f9746fc0e59f6a0a50b2881d267
tORdOBLAO <101976400+tORdOBLAO@users.noreply.github.com>
2022-06-21 13:23:52 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'adamant'
+game 'gta5'
+author 'TorEsp#2894'
+description 'ESX Menu Default V1 by TorEsp#2894'
+
+client_scripts {
+	'@es_extended/imports.lua',
+	'client/main.lua'
+}
+
+ui_page {
+	'html/ui.html'
+}
+
+files {
+	'html/ui.html',
+	'html/css/app.css',
+	'html/js/mustache.min.js',
+	'html/js/app.js'
+}
+
+dependencies {
+	'es_extended'
+}
